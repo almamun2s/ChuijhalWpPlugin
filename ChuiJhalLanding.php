de5063b49e5b9413f363597103dfcef586ae314e
Abdullah Almamun <mamun2social@gmail.com>
2024-01-24 17:07:05 +0600

    Initializing Development environment

---
+++ ChuiJhalLanding.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Plugin Name: ChuiJhal Landing
+Plugin URI: #
+Description: This plugin is made for GroceryBazar. You can make an awesome page using this plugin. 
+Version: 1.0
+Requires at least: 5.8
+Requires PHP: 5.6.20
+Author: Abdullah Almamun
+Author URI: https://github.com/almamun2s 
+License: GPLv2 or later
+Text Domain: akismet
+*/
